f47568fb65d327174ec441f2912bc119a1c78b7b
rohan <rohanaras@gmail.com>
2015-05-20 17:19:37 -0700

    fixed unknown error with c/p

--- apiRequester.py
+++ apiRequester.py
@@ -24,21 +24,23 @@ while True:
 	# break in the printed text
 	print('\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n')
 
-	# get all stops from stop-ids-for-agency method
 	stops_response = urllib.urlopen(all_stops_url)
 	all_stops = json.loads(stops_response.read())['data']['list']
 	print('number of stops: ' + str(len(all_stops)) + '\n')
 
-	# grab specific stops
-	# for stop in all_stops[0:5]:
+	# grab specific stops (0:10 is for testing purposes)
 	for stop in all_stops[0:10]:
+
 		print('Stop: ' + stop + ' {')
+
 		# get all data from arrivals-and-departures-for-stop method for current stop
-		ad_url = 'http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/ ' + stop + '.json?key=f0d2025d-3221-4cb0-860c-4466ccc1b0b0'
+		ad_url = 'http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/' + stop + '.json?key=f0d2025d-3221-4cb0-860c-4466ccc1b0b0'
 		ad_response = urllib.urlopen(ad_url)
 		ad = json.loads(ad_response.read())
+
 		# get system time as timestamp
 		current_time = ad['currentTime']
+
 		# get a specific trip near the stop
 		for ad_data in ad['data']['entry']['arrivalsAndDepartures']:
 			if ad_data['tripStatus']['predicted']:
@@ -50,10 +52,12 @@ while True:
 				scheduled_arrival = ad_data['scheduledArrivalTime']
 				predicted_departure = ad_data['predictedDepartureTime']
 				scheduled_departure = ad_data['scheduledDepartureTime']
+
 				print('\t' + route_name + ': ' + trip_sign + ' {')
 				print('\tPredicted arrival: ' + str(predicted_arrival))
 				print('\tScheduled arrival: ' + str(scheduled_arrival))
 				# predicted - sheduled gives positive lateness and negative for being early
 				print('\t}')
+
 		print('}')
 	time.sleep(3600)
